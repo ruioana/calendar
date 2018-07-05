a3e1e280fbe0df4388ed5171045d082a5132f546
ruioana <36535856+ruioana@users.noreply.github.com>
2018-07-05 17:25:32 +0100

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -1,13 +1,3 @@
-"""View the calendar
-Add an event to the calendar
-Update an existing event
-Delete an existing event
-
-Print a welcome message to the user
-Prompt the user to view, add, update, or delete an event on the calendar
-Depending on the user's input: view, add, update, or delete an event on the calendar
-The program should never terminate unless the user decides to exit"""
-
 from time import sleep, strftime
 USER_NAME = "Ioana"
 calendar = {}
