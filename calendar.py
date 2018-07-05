f607dabcd7c48ee696e0835c170826dbda981d68
ruioana <36535856+ruioana@users.noreply.github.com>
2018-07-05 17:25:03 +0100

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,76 @@
+"""View the calendar
+Add an event to the calendar
+Update an existing event
+Delete an existing event
+
+Print a welcome message to the user
+Prompt the user to view, add, update, or delete an event on the calendar
+Depending on the user's input: view, add, update, or delete an event on the calendar
+The program should never terminate unless the user decides to exit"""
+
+from time import sleep, strftime
+USER_NAME = "Ioana"
+calendar = {}
+
+def welcome():
+  print ("Welcome, " + USER_NAME + ".")
+  print ("Calendar starting...")
+  sleep(1)
+  print ("Today is: " + strftime("%A %B %d, %Y"))
+  print ("The time is: " + strftime("%H:%M:%S"))
+  sleep(1)
+  print ("What would you like to do?")
+  
+  
+def start_calendar():
+  welcome()
+  start = True
+  while start:
+    user_choice = input("A to Add, U to Update, V to View, D to Delete, X to Exit ")
+    user_choice = user_choice.upper()
+    if user_choice == "V":
+      if len(calendar.keys()) < 1:
+        print ("Calendar empty.")
+      else:
+        print (calendar)
+    elif user_choice == "U":
+          date = input("What date? ")
+          update = input("Enter the update: ")
+          calendar[date] = update
+          print ("Your calendar has been updated")
+          print (calendar)
+    elif user_choice == "A":
+          event = input("Enter event: ")
+          date = input("Enter date (MM/DD/YYYY): ")
+          if (len(date) > 10 or int(date[6:]) < int(strftime("%Y"))):
+            print ("Invalid date ...")
+            try_again = input("Try Again? Y for Yes, N for No: ")
+            try_again = try_again.upper()
+            if try_again == "Y":
+              continue
+            else:
+              start = False
+          else:
+            calendar[date] = event
+            print ("The event was added successfully.")
+            print (calendar)
+    elif user_choice == "D":
+          if len(calendar.keys()) < 1:
+            print ("Your calendar is empty")
+          else:
+            event = input("What event? ")
+            for date in calendar.keys():
+              if event == calendar[date]:
+                del calendar[date]
+                print ("Your entry was deleted")
+                print (calendar)
+              else:
+                print ("Incorrect entry ..")
+    elif user_choice == "X":
+          start = False
+    else:
+      print ("Invalid command!")
+      start = False
+
+start_calendar()
+  
